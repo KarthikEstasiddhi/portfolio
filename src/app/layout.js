201e97f805056e4e934a6f213822a909cea8e9c4
KarthikEstasiddhi <karthik.22bce7997@vitapstudent.ac.in>
2026-02-04 09:55:23 +0530

    changed layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "CS Student Portfolio",
+  title: "Karthik Portfolio",
   description: "Portfolio detailing research, projects, and experience.",
 };
 
